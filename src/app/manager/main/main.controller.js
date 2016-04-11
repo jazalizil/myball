c2a970ac096296ad2661e5a0debd9f5109bf8c3f
jazalizil <dabbechjalil@yahoo.fr>
2016-04-11 08:27:33 +0800

    correctifs vitrine

--- src/app/manager/main/main.controller.js
+++ src/app/manager/main/main.controller.js
@@ -3,6 +3,16 @@
   angular.module('myBall').controller('MainController', MainController);
 
   /** @ngInject */
-  function MainController() {
+  function MainController(gettextCatalog, UserService, Conf) {
+    var vm = this;
+    vm.data = {
+      identity: UserService.getIdentity(),
+      imgs: [{
+        src: Conf.CDN_BASE_URL + "images/footsalle-field.png"
+      }]
+    };
+    vm.data.imgs[0].titles = [
+      vm.data.identity.five.name
+    ];
   }
 })();
